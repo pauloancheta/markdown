52991c4321299789aefe6a1add85e05d627d15a8
pauloancheta <paulo.ancheta@gmail.com>
2017-09-11 12:33:37 +1000

    format all headings

--- lib/markdown.ex
+++ lib/markdown.ex
@@ -1,4 +1,13 @@
 defmodule Markdown do
+  @formatter_map %{
+    "#" => "h1",
+    "##" => "h2",
+    "###" => "h3",
+    "####" => "h4",
+    "#####" => "h5",
+    "######" => "h6",
+  }
+
   def call(file) do
     file
     |> String.split("\n")
@@ -13,4 +22,21 @@ defmodule Markdown do
     format_to_html(tail, list ++ [format(head)])
   end
   def format_to_html([], result), do: result
+
+  defp format(string) do
+    {formatter, parsed_string} = find_formatter(string)
+
+    "<#{formatter}>#{parsed_string}</#{formatter}>"
+  end
+
+  defp find_formatter(string) do
+    [formatter | tail] = string |> String.split
+
+    case Map.fetch(@formatter_map, formatter) do
+      {:ok, formatter} ->
+        {formatter, Enum.join(tail, " ")}
+      :error ->
+        {"p", string}
+    end
+  end
 end
--- test/markdown_test.exs
+++ test/markdown_test.exs
@@ -9,17 +9,33 @@ defmodule MarkdownTest do
     assert count == 2
   end
 
-  @tag :skip
-  test "test paragraphs" do
+  test "paragraphs" do
     test_string = "a\nb"
     assertion = ["<p>a</p>", "<p>b</p>"]
     assert Markdown.call(test_string) == assertion
   end
 
-  @tag :skip
-  test "test heading 1" do
+  test "heading 1" do
     test_string = "# a\nb"
     assertion = ["<h1>a</h1>", "<p>b</p>"]
+    assert Markdown.call(test_string) == assertion
+  end
+
+  test "heading 1 with multiple words in one line" do
+    test_string = "# a 1\nb"
+    assertion = ["<h1>a 1</h1>", "<p>b</p>"]
+    assert Markdown.call(test_string) == assertion
+  end
+
+  test "heading 2 with multipile words" do
+    test_string = "## a 1\nb"
+    assertion = ["<h2>a 1</h2>", "<p>b</p>"]
+    assert Markdown.call(test_string) == assertion
+  end
+
+  test "invalid formatter" do
+    test_string = "##a 1\nb"
+    assertion = ["<p>##a 1</p>", "<p>b</p>"]
     assert Markdown.call(test_string) == assertion
   end
 end
